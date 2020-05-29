78f9e701796287cddfcaaff05d73de0c81ccef16
ShityBrain <65502219+ShityBrainOfficial@users.noreply.github.com>
2020-05-29 17:53:59 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,3 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -6,13 +5,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Guide.](https://how2techy.com/xtra-guide1/)"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "WoW, u don't even know how to set a Name on Heroku..."
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**My Userbot is online, is yours too?**\n\n"
-                     "Telethon version: 6.9.0\nPython: 3.7.3\n"
-                     "This userbot was modified by [ShityBrain](t.me/ShityBrainOfficial)"
-                     f"I currently run on the User {DEFAULTUSER}\n\n"
-                     "[Click here](https://github.com/ShityBrainOfficial/) to open this userbot's repo")
+    await alive.client.send_message(
+            alive.chat_id,
+            f"Python Version: 3.7.4\nHeroku-Dyno: Premium-2\n\nThis lit shit was made by [ShityBrain](https://t.me/ShityBrainOfficial)\nI currently run on {DEFAULTUSER}",
+            file="https://i.imgur.com/T60vNl1.jpg"
+        )
+    await alive.delete()
